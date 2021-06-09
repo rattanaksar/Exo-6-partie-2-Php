b55c574b27314b91b30e0497876f2220be9514bb
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 13:26:44 +0200

    Exo 6 partie 2 PHP

---
+++ Exo 6 partie 2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 6 : Les conditions</title>
+</head>
+<body>
+    <h1>Exo 6 : Les conditions</h1>
+    <h4>Traduire ce code avec des if et des else : </h4>
+    <?php
+    $age = 17;
+    if ($age >= 18) {
+        echo"Tu es majeur";
+    } else {
+        echo"Tu es mineur";
+    }
+    ?>
+</body>
+</html>
